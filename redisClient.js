d75320d10e374353c81665928141ae4c30a7dfec
Pawan Malhi <pawankumar00@live.com>
2025-11-01 01:11:38 -0400

    Redis server connection

---
+++ redisClient.js
@@ -0,0 +1,10 @@
+import { createClient } from "redis";
+
+const client = createClient({ url: "redis://localhost:6379" });
+
+client.on("error", (err) => console.error("❌ Redis Client Error", err));
+
+await client.connect();
+console.log("✅ Connected to Redis");
+
+export default client;
